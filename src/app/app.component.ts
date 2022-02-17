59dd02b26b1aebc1ddc6752b3f2deb583198bdae
joseph <joseph@X6>
2022-02-17 15:47:04 +0000

    Added sorting of articles, refactoring.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,8 @@ export class AppComponent {
     console.log(`${title.value} ${link.value}`)
     return false;
   }
+  
+  sortedArticles(): article[]{
+    return this.articles.sort((a:article, b: article) => b.votes - a.votes);
+  }
 }
